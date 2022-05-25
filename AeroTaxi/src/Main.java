3096fe731f219eae9bf7fa77c69ec6c5ca008562
LChapita <92611605+LChapita@users.noreply.github.com>
2022-05-25 19:45:19 -0300

    Nuevas Clases

--- AeroTaxi/src/Main.java
+++ AeroTaxi/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("AeroTaxi");
-        System.out.println("Lucas");
-        System.out.println("macacooooo");
-        System.out.println("tengo un hambre");
-        System.out.println("prueba intelliJ");
+        System.out.println("verificar 1");
     }
 }
