1f84b6e60df7576355b2521da11b8d669fd82812
Leonardo <leonardosuarezhans@gmail.com>
2022-06-10 21:40:13 -0300

    UserMenu terminado

--- AeroTaxi/src/Menu/UserMenu.java
+++ AeroTaxi/src/Menu/UserMenu.java
@@ -1,9 +1,9 @@
 package Menu;
 
-
-
 import User.RegisterAndLogin;
+import User.User;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,48 +11,48 @@ public class UserMenu{
     public UserMenu(){}
 
 
-    public void Options(){
-        RegisterAndLogin userMenu = new RegisterAndLogin();
-
-        System.out.println("\n1 - Register: \n");
-        System.out.println("2 - Login: \n");
-        System.out.println("3 - Exit: \n");
-
+    public void Options(ArrayList<User> userArrayList){
+        RegisterAndLogin userCreation = new RegisterAndLogin();
+        User user = new User();
         Scanner scanner = new Scanner(System.in);
 
         int option = 0;
-        try {
-            option = scanner.nextInt();
-        } catch(InputMismatchException e){
-            System.out.println("Only integer value");
+        while(option != 3) {
+
+            System.out.println("\n1 - Register: \n");
+            System.out.println("2 - Login: \n");
+            System.out.println("3 - Exit: \n");
+
+            try {
+                option = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Only integer value");
+            }
+
+
+            switch (option) {
+                case 1:
+                    userCreation.Register(user, userArrayList);
+                    break;
+
+                case 2:
+                    userCreation.Login(user, userArrayList);
+                    break;
+
+                case 3:
+                    exitOption();
+                    break;
+            }
         }
-
-
-        switch(option){
-            case 1:
-                userMenu.Register();
-                break;
-
-            case 2:
-                //userMenu.Login();
-                break;
-
-
-
-
-        }
-
-
-
-
-
-
 
     }
 
 
 
 
+    public void exitOption(){
+        System.out.println("Finish process");
+    }
 
 
 }
